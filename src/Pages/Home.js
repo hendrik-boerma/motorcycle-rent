f7782093688f0a57b30093ff403605c7bfe7dde2
Hendrik Boerma <hendrikboerma@gmail.com>
2024-04-12 16:40:57 +0200

    random bikes homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,21 @@
 import './Home.css';
 import Title from '../Components/Title';
+import Cards from '../Components/Cards';
+import { bikes } from '../Data.js'
 
 function Home() {
+
+    function getRandomElement(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }    
+
+    const bikesHome = [getRandomElement(bikes), getRandomElement(bikes), getRandomElement(bikes)];
+    console.log(bikesHome);
+
     return (
         <>
-            <Title title="Rent your dream Harley" description="We have a wide range of Harleys that you can rent. Please take a look and have a beautiful ride."/>
+            <Title title="Rent your dream bike" description=""/>
+            <Cards products={bikesHome} brand={'All brands'} price={0} />
         </>
 
     );
